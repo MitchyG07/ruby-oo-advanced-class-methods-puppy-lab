0e4cae26dd42b32a70c938185129f0085f7e85e8
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-18 23:27:24 -0800

    commit tree 96fe1f5e2f433d744ce86dd10cc5cdf68e31a35b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+    attr_accessor :name, :save
+
+    @@all = [] 
+    @@name = []
+
+    def initialize(name)
+        save 
+        @name = name
+        # binding.pry
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def self.clear_all 
+        @@all.clear
+    end 
+
+    def self.print_all 
+        @@all.each { |x| puts x.name }
+    end 
+
+    def save
+       @@all << self
+    end 
+
+end 
